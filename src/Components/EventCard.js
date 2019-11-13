76ff46e076f5b79c05d083d191a126831a4d13ee
samirahisham <samhisham95@gmail.com>
2019-11-13 12:54:13 +0300

    Final

--- src/Components/EventCard.js
+++ src/Components/EventCard.js
@@ -9,7 +9,7 @@ const EventCard = (props) => {
     <div className="col-lg-4 col-sm-6 mb-3 mt-5">
       <Link to={`/events/${event.id}`}>
         <div className=" eventlistshadow card h-100">
-          <img className="card-img-top" src={event.img} alt={event.img} />
+          <img className="card-img-top" src={event.img} alt={event.img} style={{height:275}} />
           <div className="card-body">
             <h4 className="card-title">{event.title.toUpperCase()}</h4>
             <p className="card-text">{`${event.location} - ${event.date}`}</p>
